68d2476c8d400fa4b56757d5d7b621f952e5135b
jesonpro <88507137+jesonpro@users.noreply.github.com>
2021-09-04 00:36:11 +0530

    Update worktype.js

--- plugins/worktype.js
+++ plugins/worktype.js
@@ -1,7 +1,8 @@
-/* Copyright (C) 2021 afnanplk.
-re-coded by TERROR BOY
+/* Copyright (C) 2021 TENUX-Neotro.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+NEOTROX - TEENUHX
 */
-
 const Asena = require('../events');
 const config = require('../config');
 const Heroku = require('heroku-client');
@@ -16,19 +17,19 @@ let baseURI = '/apps/' + config.HEROKU.APP_NAME;
  var W_ADM = ''
   if (config.LANG == 'EN') {
 
-    W_ADM = 'ᴡᴏʀᴋ ᴛʏᴘᴇ ɪꜱ ᴀᴅᴍɪɴ ɴᴏᴡ' 
+    W_ADM = 'ᴡᴏʀᴋ ᴛʏᴘᴇ ɪꜱ ᴘʀɪᴠᴀᴛᴇ ɴᴏᴡ' 
     W_PUB = 'ᴡᴏʀᴋ_ᴛʏᴘᴇ ɪꜱ ɴᴏᴡ ᴘᴜʙʟɪᴄ'
     W_PRI = 'ᴡᴏʀᴋ_ᴛʏᴘᴇ ɪꜱ ɴᴏᴡ ᴘʀɪᴠᴀᴛᴇ'
     }
 
-    if (config.LANG == 'ML') {
+    if (config.LANG == 'SI') {
 
-      W_ADM = 'ᴡᴏʀᴋ ᴛʏᴘᴇ ɪꜱ ᴀᴅᴍɪɴ ɴᴏᴡ'
-      W_PUB = 'ᴡᴏʀᴋ_ᴛʏᴘᴇ ɪꜱ ɴᴏᴡ ᴘᴜʙʟɪᴄ'
-      W_PRI = 'ᴡᴏʀᴋ_ᴛʏᴘᴇ ɪꜱ ɴᴏᴡ ᴘʀɪᴠᴀᴛᴇ'
+      W_ADM = 'ඔබේ ʙᴏᴛ පෞද්ගලික අකාරයට ක්‍රියාත්මක විය.'
+      W_PUB = 'ඔබෙ ʙᴏᴛ පෞද්ගලික අකාරයට ක්‍රියාත්මක විය'
+      W_PRI = 'ඔබේ ʙᴏᴛ පෞද්ගලික අකාරයට ක්‍රියාත්මක විය.'
     }
 
- Asena.addCommand({pattern: 'work ?(.*)', fromMe: true,dontAddCommandList: true,desc: 'change bot work type. example - .work public/private/admin' }, (async (message, match) => {
+ Asena.addCommand({pattern: 'work ?(.*)', fromMe: true,dontAddCommandList: true,desc: 'change bot work type. example - .work public/private' }, (async (message, match) => {
         if (match[1] == 'public') {
                 await heroku.patch(baseURI + '/config-vars', { 
                     body: { 
@@ -46,7 +47,7 @@ let baseURI = '/apps/' + config.HEROKU.APP_NAME;
          } else if (match[1] == 'private') {
                 await heroku.patch(baseURI + '/config-vars', { 
                     body: { 
-                        ['WORK_TYPE']: 'admin'
+                        ['WORK_TYPE']: 'private'
                     } 
                 });
                 await message.sendMessage(W_ADM)
