ca38a70e336d48ad8515a329fd3a68dfd1bb6c7e
jesonpro <88507137+jesonpro@users.noreply.github.com>
2021-09-04 12:33:59 +0530

    Update groupinfo.js

--- plugins/groupinfo.js
+++ plugins/groupinfo.js
@@ -1,9 +1,7 @@
-/* Copyright (C) 2021 Hisham-muhammed.
-
-Re-coded by TERROR-BOY
-
+/* Copyright (C) 2021 TENUX-Neotro.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
+NEOTROX - TEENUHX
 */
 
 const Asena = require('../events');
