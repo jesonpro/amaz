fddc3ab3d17054abb6d4c27f2ce8b4d0dbc8c379
jesonpro <88507137+jesonpro@users.noreply.github.com>
2021-09-04 16:51:26 +0530

    Update StringSession.js

--- alexa/StringSession.js
+++ alexa/StringSession.js
@@ -11,7 +11,7 @@ class StringSession {
     }
 
     deCrypt(string = undefined) {
-        if ('NEUTRO_SESSION' in process.env && string === undefined) {
+        if ('AMAZONE_SESSION' in process.env && string === undefined) {
             string = process.env.STRING_SESSION;
         } else if (string !== undefined) {
             if (fs.existsSync(string)) {
